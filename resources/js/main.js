d64b024cf4d7e3fbad4748be9e52967953fcdf53
Kenny Lim <5193560+kennylim924@users.noreply.github.com>
2018-02-08 21:09:50 -0600

    complete jQuery functions and project

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,4 +10,16 @@ $(document).ready(function(){
   });
 });
 
-var userScroll = 0;
+// Set Scroll
+var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
+  });
